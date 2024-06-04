e0a3d68eaac361984d504fd0eeb23a55fb5615b7
Geovanny <birdcode.py@gmail.com>
2024-06-03 22:39:09 -0500

    Program controls and movement

--- characters.py
+++ characters.py
@@ -9,6 +9,11 @@ class Character():
 		self.shape = pygame.Rect(0, 0, constants.WIDTH_CHARACTER, constants.HEIGHT_CHARACTER)
 		self.shape.center = (x, y)
 
+	def movement(self, delta_x, delta_y):
+
+		self.shape.x = self.shape.x + delta_x
+		self.shape.y = self.shape.y + delta_y
+
 	def draw(self, interface):
 
 		pygame.draw.rect(interface, constants.COLOR, self.shape)
--- main.py
+++ main.py
@@ -11,9 +11,50 @@ pygame.init()
 window = pygame.display.set_mode((constants.WIDTH_WINDOW, constants.HEIGHT_WINDOW))
 pygame.display.set_caption("TreeCode Crypto")
 
+# Definir variables de movimiento del jugador
+
+move_up = False
+move_down = False
+move_right = False
+move_left = False
+
+# Controlar el frame rate
+reloj = pygame.time.Clock()
+
 run = True
 
 while run == True:
+
+	# Que vaya a 60 FPS
+	reloj.tick(constants.FPS)
+
+
+
+	window.fill(constants.COLOR_BG)
+
+	# Calcular el movimiento del jugador
+
+	delta_x = 0
+	delta_y = 0
+
+	if move_right == True:
+
+		delta_x = constants.VELOCITY
+
+	if move_left == True:
+
+		delta_x = -constants.VELOCITY
+
+	if move_up == True:
+
+		delta_y = -constants.VELOCITY
+
+	if move_down == True:
+
+		delta_y = constants.VELOCITY
+
+	# Mover al jugador
+	player.movement(delta_x, delta_y)
 
 	player.draw(window)
 
@@ -22,6 +63,46 @@ while run == True:
 		if event.type == pygame.QUIT:
 
 			run = False
+
+		if event.type == pygame.KEYDOWN:
+
+			if event.key == pygame.K_a:
+
+				#print("Left")
+				move_left = True
+
+			if event.key == pygame.K_d:
+
+				#print("Right")
+				move_right = True
+
+			if event.key == pygame.K_w:
+
+				#print("Up")
+				move_up = True
+
+			if event.key == pygame.K_s:
+
+				#print("Down")
+				move_down = True
+
+		if event.type == pygame.KEYUP:
+
+			if event.key == pygame.K_a:
+
+				move_left = False
+
+			if event.key == pygame.K_d:
+
+				move_right = False
+
+			if event.key == pygame.K_w:
+
+				move_up = False
+
+			if event.key == pygame.K_s:
+
+				move_down = False
 
 
 	pygame.display.update()
